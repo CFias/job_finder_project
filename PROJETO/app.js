98f2e69d74e5f814c9a7867ccb2ae7a30def5692
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-14 21:16:33 -0300

    hbs and jobs

--- PROJETO/app.js
+++ PROJETO/app.js
@@ -1,7 +1,10 @@
 const express    = require('express');
+const exphbs     = require('express-handlebars');
 const app        = express();
+const path       = require('path');
 const db         = require('./db/connection');
 const bodyParser = require('body-parser');
+const Job        = require('./models/Job');
 
 const PORT = 3000;
 
@@ -11,6 +14,14 @@ app.listen(PORT, function() {
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// static folder 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // db Connection
 db
@@ -24,7 +35,18 @@ db
 
 // Routes
 app.get('/', (req, res) => {
-    res.send("Está funcionando 2");
+
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+
+      res.render('index', {
+        jobs
+      });
+      
+    });
+
 });
 
 // jobs routes 
